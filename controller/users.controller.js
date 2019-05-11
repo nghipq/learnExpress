5aba1b6d3b6d0a4009860d7418e2c5a58ac10ee3
nghipq <nghipqce140179@fpt.edu.vn>
2019-05-11 18:56:10 +0700

    make error!

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -32,6 +32,18 @@ module.exports.get = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
     req.body.id = shortid.generate();
+    var errors = [];
+    if(!req.body.name) {
+        errors.push('name is required!');
+    }
+
+    if(errors.length) {
+        res.render('users/create', {
+            errors: errors,
+            values: req.body
+        });
+        return;
+    }
     db.get('users').push(req.body).write();
     res.redirect('/user');
 }
