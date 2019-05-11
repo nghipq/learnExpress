caae267d09252c4d0a8e69174a6f0d2c04c9c95c
nghipq <nghipqce140179@fpt.edu.vn>
2019-05-11 19:23:19 +0700

    get middleware

---
+++ middleware/users,validate.js
@@ -0,0 +1,16 @@
+module.exports.postCreate = function(req, res, next) {
+    var errors = [];
+    if(!req.body.name) {
+        errors.push('name is required!');
+    }
+
+    if(errors.length) {
+        res.render('users/create', {
+            errors: errors,
+            values: req.body
+        });
+        return;
+    }
+    next()
+}
+
